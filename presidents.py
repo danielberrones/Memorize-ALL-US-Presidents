afc346ed01a5e857715af3a2e81588ea26244470
DBerrones <53327546+danielberrones@users.noreply.github.com>
2021-11-11 22:07:38 -0600

    Refactored

--- presidents.py
+++ presidents.py
@@ -2,124 +2,132 @@ from random import randint
 from sys import exit
 from time import sleep
 
-presidents = {
-    1: "George Washington",
-    2: "John Adams",
-    3: "Thomas Jefferson",
-    4: "James Madison",
-    5: "James Monroe",
-    6: "John Quincy Adams",
-    7: "Andrew Jackson",
-    8: "Martin Van Buren",
-    9: "William Henry Harrison",
-    10: "John Tyler",
-    11: "James K. Polk",
-    12: "Zachary Taylor",
-    13: "Millard Fillmore",
-    14: "Franklin Pierce",
-    15: "James Buchanan",
-    16: "Abraham Lincoln",
-    17: "Andrew Johnson",
-    18: "Ulysses S. Grant",
-    19: "Rutheford B. Hayes",
-    20: "James Garfield",
-    21: "Chester Arthur",
-    22: "Grover Cleveland",
-    23: "Benjamin Harrison",
-    24: "Grover Cleveland",
-    25: "William McKinley",
-    26: "Theodore Roosevelt",
-    27: "William Howard Taft",
-    28: "Woodrow Wilson",
-    29: "Warren Harding",
-    30: "Calvin Coolidge",
-    31: "Herbert Hoover",
-    32: "Franklin D. Roosevelt",
-    33: "Harry S. Truman",
-    34: "Dwight D. Eisenhower",
-    35: "John F. Kennedy",
-    36: "Lyndon B. Johnson",
-    37: "Richard Nixon",
-    38: "Gerald Ford",
-    39: "Jimmy Carter",
-    40: "Ronald Reagan",
-    41: "George H.W. Bush",
-    42: "Bill Clinton",
-    43: "George W. Bush",
-    44: "Barack Obama",
-    45: "Donald Trump",
-    46: "Joe Biden",
-}
+class Game:
+    def __init__(self):
+        self.hintCounter = 0
+        self.correct = []
+        self.wrong = []
+        self.presidents = {
+            1: "George Washington",
+            2: "John Adams",
+            3: "Thomas Jefferson",
+            4: "James Madison",
+            5: "James Monroe",
+            6: "John Quincy Adams",
+            7: "Andrew Jackson",
+            8: "Martin Van Buren",
+            9: "William Henry Harrison",
+            10: "John Tyler",
+            11: "James K. Polk",
+            12: "Zachary Taylor",
+            13: "Millard Fillmore",
+            14: "Franklin Pierce",
+            15: "James Buchanan",
+            16: "Abraham Lincoln",
+            17: "Andrew Johnson",
+            18: "Ulysses S. Grant",
+            19: "Rutheford B. Hayes",
+            20: "James Garfield",
+            21: "Chester Arthur",
+            22: "Grover Cleveland",
+            23: "Benjamin Harrison",
+            24: "Grover Cleveland",
+            25: "William McKinley",
+            26: "Theodore Roosevelt",
+            27: "William Howard Taft",
+            28: "Woodrow Wilson",
+            29: "Warren Harding",
+            30: "Calvin Coolidge",
+            31: "Herbert Hoover",
+            32: "Franklin D. Roosevelt",
+            33: "Harry S. Truman",
+            34: "Dwight D. Eisenhower",
+            35: "John F. Kennedy",
+            36: "Lyndon B. Johnson",
+            37: "Richard Nixon",
+            38: "Gerald Ford",
+            39: "Jimmy Carter",
+            40: "Ronald Reagan",
+            41: "George H.W. Bush",
+            42: "Bill Clinton",
+            43: "George W. Bush",
+            44: "Barack Obama",
+            45: "Donald Trump",
+            46: "Joe Biden"
+            }   
 
-correct = []
-wrong = []
-hintCounter = 0
+    def clearScreen(self, lines):
+        lines = int(lines)
+        print('\n' * lines)
 
-def listPres():
-    v = [v for v in presidents.values()]
-    for x in v:
-        print(x)
+    def exitGame(self):
+        exit()
 
-def printIntro():
-    print("Learn the U.S. Presidents")
-    sleep(1)
+    def listPres(self):
+        v = [v for v in self.presidents.items()]
+        print(v)
 
-printIntro()
+    def printIntro(self):
+        self.clearScreen(2)
+        print("Learn the U.S. Presidents!")
 
-def showMenu():
-    print("Menu")
-    print("------------------------")
-    print("'score'- show current score")
-    print("'pres' - show all presidents")  
-    print("'exit' - exit game")
-    print("'menu' - show menu")
-    print("'clear' - clear screen")
-    print("------------------------")
-    input("Hit Enter to continue...")
+    def showMenu(self):
+        print("\n[Menu]")
+        print("'clear' - clear screen")
+        sleep(.5)
+        print("'exit' - exit game")
+        sleep(.5)
+        print("'menu' - show menu")
+        sleep(.5)
+        print("'pres' - show all presidents")  
+        sleep(.5)
+        print("'score'- show current score")
+        sleep(.5)
+        input("\nHit enter to continue game...")
 
-showMenu()
+    def showStats(self):
+        self.clearScreen(3)
+        print(f'Total Hints: {self.hintCounter}\n')
+        print(f'Total Correct: {len(self.correct)},\n{self.correct}\n')
+        print(f'Total Wrong: {len(self.wrong)}, \n{self.wrong}')
 
-def clearScreen():
-    print('\n'*50)
-
-def showStats():
-    print(f'Total Hints: {hintCounter}\n')
-    print(f'Total Correct: {len(correct)},\n{correct}\n')
-    print(f'Total Wrong: {len(wrong)}, \n{wrong}')
 
 def main():
+    game = Game()
+    game.printIntro()
+    game.showMenu()
+
     while True:
         ranNum = randint(1,46)
-        correctAnswer = presidents[ranNum]
+        correctAnswer = game.presidents[ranNum]
         userResponse = input(f'\nU.S. President #{ranNum}: \n')
 
         if userResponse == "score":
-            showStats()
+            game.showStats()
         elif userResponse == "pres":
-            listPres()
+            game.listPres()
         elif userResponse == "exit":
-            showStats()
-            exit()
+            game.showStats()
+            game.exitGame()
         elif userResponse == "menu":
-            showMenu()
+            game.showMenu()
         elif userResponse == "clear":
-            clearScreen()
+            game.clearScreen(35)
         elif userResponse == correctAnswer:
             tup = (userResponse,ranNum)
-            correct.append(tup)
+            game.correct.append(tup)
             print("Correct!\n")
         else:
             tup = (userResponse,ranNum)
-            wrong.append(tup)
-            print(presidents.get((userResponse),"Wrong."))
+            game.wrong.append(tup)
+            print(game.presidents.get((userResponse),"Wrong."))
 
             hintOrNo = input("Need a hint? (y/n)\n")
             if hintOrNo == 'y':
-                hintCounter += 1 
+                game.hintCounter += 1 
                 print(correctAnswer)
             elif hintOrNo == 'n':
                 continue
-                
-               
+
 
 main()
